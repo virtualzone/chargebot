b16dba0b8cd5e482780a45a2a3ea4e38ee9a6da9
virtualzone <13085858+virtualzone@users.noreply.github.com>
2023-11-21 19:29:48 +0100

    Auth flow

--- auth-router.go
+++ auth-router.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+type TokenReponse struct {
+	RefreshToken string `json:"refresh_token"`
+	AccessToken  string `json:"access_token"`
+}
+
 func GetAuthRouterInitThirdParty(w http.ResponseWriter, r *http.Request) {
 	code := CreateAuthCode()
 
@@ -32,20 +37,26 @@ func GetAuthRouterInitThirdParty(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetAuthRouterCallback(w http.ResponseWriter, r *http.Request) {
-	code := r.URL.Query().Get("code")
-	if !IsValidAuthCode(code) {
+	state := r.URL.Query().Get("state")
+	if !IsValidAuthCode(state) {
 		SendNotFound(w)
 		return
 	}
-	getTokens(code)
+	tokens, err := getTokens(r.URL.Query().Get("code"))
+	if err != nil {
+		log.Println(err)
+		SendBadRequest(w)
+		return
+	}
+	log.Println(tokens)
 }
 
-func getTokens(code string) {
+func getTokens(code string) (*TokenReponse, error) {
 	target := "https://auth.tesla.com/oauth2/v3/token"
 	data := url.Values{}
 	data.Set("grant_type", "authorization_code")
 	data.Set("client_id", GetConfig().ClientID)
-	data.Set("client_secret", "")
+	data.Set("client_secret", GetConfig().ClientSecret)
 	data.Set("code", code)
 	data.Set("redirect_uri", "https://"+GetConfig().Hostname+"/api/1/auth/callback")
 	data.Set("audience", GetConfig().Audience)
@@ -53,6 +64,16 @@ func getTokens(code string) {
 	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	client := &http.Client{}
-	resp, _ := client.Do(r)
-	log.Println(resp)
+	resp, err := client.Do(r)
+	if err != nil {
+		// TODO
+		log.Println(err)
+		return nil, err
+	}
+
+	var m TokenReponse
+	if err := UnmarshalValidateBody(resp.Body, &m); err != nil {
+		return nil, err
+	}
+	return &m, nil
 }
